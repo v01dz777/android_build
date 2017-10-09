3a1f8240e4b81bc08c453bb456b5d445d5985634
voidz777 <voidz.evo3d@gmail.com>
2017-10-09 14:36:19 +0900

    Revert "build: Add Lineage build support"
    This reverts commit b9c773db6622a380963dac68f130ec451f565aae.

--- core/config.mk
+++ core/config.mk
@@ -890,16 +890,9 @@ include $(BUILD_SYSTEM)/ninja_config.mk
 include $(BUILD_SYSTEM)/soong_config.mk
 endif
 
-ifneq ($(LINEAGE_BUILD),)
-## We need to be sure the global selinux policies are included
-## last, to avoid accidental resetting by device configs
-$(eval include device/lineage/sepolicy/common/sepolicy.mk)
+include $(TOPDIR)vendor/extras/core/dumpvar.mk
 
-# Include any vendor specific config.mk file
--include $(TOPDIR)vendor/*/build/core/config.mk
-
-# Include any vendor specific apicheck.mk file
--include $(TOPDIR)vendor/*/build/core/apicheck.mk
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+include $(TOPDIR)vendor/extras/core/pathmap.mk
+include $(TOPDIR)vendor/extras/core/qcom_target.mk
 endif
-
-include $(BUILD_SYSTEM)/dumpvar.mk

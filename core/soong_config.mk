93c56e98f78ff3936e21a28068cc40b8295d49c2
voidz777 <voidz.evo3d@gmail.com>
2017-10-09 14:40:00 +0900

    Revert "soong: include lineage board flags"
    This reverts commit bb51488e93bc9dd210dba06db9c351ea0c34ac10.

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -16,8 +16,8 @@ endif
 # Create soong.variables with copies of makefile settings.  Runs every build,
 # but only updates soong.variables if it changes
 SOONG_VARIABLES_TMP := $(shell mktemp -u)
-include vendor/lineage/build/soong/soong_config.mk
-$(SOONG_VARIABLES): FORCE lineage_soong
+include vendor/extras/soong/soong_config.mk
+$(SOONG_VARIABLES): FORCE du_soong
 	$(hide) (\
 	echo '    "Make_suffix": "-$(TARGET_PRODUCT)",'; \
 	echo ''; \
